d26cf5084d4fefef13337ee0e9f4ab787f321fbe
Destaby <rudik.nick.nick@gmail.com>
2021-12-02 14:18:26 +0200

    add test

---
+++ test/root.spec.js
@@ -0,0 +1,8 @@
+const server = require('../server');
+const request = require('supertest');
+
+describe('Server', () => {
+  it('User fisits home page', () => {
+    return request(server).get('/').expect(200).expect('Helloasd World!');
+  });
+});
