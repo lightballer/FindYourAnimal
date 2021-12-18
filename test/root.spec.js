b7e54d95d9e62c7f8cc0fcd315befb57fd4ed37e
Destaby <rudik.nick.nick@gmail.com>
2021-12-18 17:28:58 +0200

    test hooks

--- test/root.spec.js
+++ test/root.spec.js
@@ -1,6 +1,7 @@
 const server = require('../server');
 const request = require('supertest');
 
+// comment
 describe('Server', () => {
   it('User fisits home page', () => {
     return request(server).get('/').expect(200).expect('Hello World!');
